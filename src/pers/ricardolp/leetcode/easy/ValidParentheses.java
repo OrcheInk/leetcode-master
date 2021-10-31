d983ba49d8351c48c2bc924ce582ba0eb7bd2eb7
RicardoLP <861640471@qq.com>
2021-10-31 14:33:16 +0800

    feat: add new solutions.

---
+++ src/pers/ricardolp/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,188 @@
+package pers.ricardolp.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * leetcode_20 : Valid Parentheses.
+ * <p>
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * <pre>
+ * 1. Open brackets must be closed by the same type of brackets.
+ * 2. Open brackets must be closed in the correct order.
+ *
+ * Example 1:
+ * Input: s = "()"
+ * Output: true
+ *
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ *
+ * Example 3:
+ * Input: s = "(]"
+ * Output: false
+ *
+ * Example 4:
+ * Input: s = "([)]"
+ * Output: false
+ *
+ * Example 5:
+ * Input: s = "{[]}"
+ * Output: true
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ * </pre>
+ *
+ * @author RicardoLP
+ * @since 2021/10/30
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "([)]";
+        System.out.println(isValid3(s));
+    }
+
+    /**
+     * Solution1.
+     * <p>
+     * Judging the validity of parentheses can be solved by using the data structure
+     * "stack". After the traversal is over, if there is no left parenthesis in the
+     * stack, it means that we close all the left parentheses in the string s and
+     * return {@code true}, otherwise return {@code false}.
+     *
+     * @param s the string to be verified.
+     * @return is the string valid.
+     */
+    private static boolean isValid1(String s) {
+        int length = s.length();
+        // If the string length is odd, return false.
+        if (length % 2 == 1) {
+            return false;
+        }
+        // In order to quickly determine the type of parentheses, we can use a hash
+        // table to store each type of parentheses.
+        HashMap<Character, Character> hash = new HashMap<>(3);
+        hash.put(')', '(');
+        hash.put('}', '{');
+        hash.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (hash.containsKey(ch)) {
+                // Determine whether the current bracket matches the top of the stack.
+                if (stack.isEmpty() || !stack.peek().equals(hash.get(ch))) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * Solution2.
+     * <p>
+     * Use arrays to simulate stack operations.
+     *
+     * @param s the string to be verified.
+     * @return is the string valid.
+     */
+    private static boolean isValid2(String s) {
+        // If the string length is odd, return false.
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        char[] array = new char[s.length() + 1];
+        int count = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                // Push.
+                array[count++] = c;
+            } else {
+                // Pop.
+                count--;
+                if (array[count] != '(' && c == ')') {
+                    return false;
+                }
+                if (array[count] != '[' && c == ']') {
+                    return false;
+                }
+                if (array[count] != '{' && c == '}') {
+                    return false;
+                }
+            }
+        }
+        return count == 1;
+    }
+
+    /**
+     * Solution3!
+     * <p>
+     * Use ASCII as the judgment condition.
+     * <p>
+     * Use arrays to simulate stack operations.
+     *
+     * <pre>
+     *      '(' :   40
+     *      ')' :   41
+     *      '[' :   91
+     *      ']' :   93
+     *      '{' :   123
+     *      '}' :   125
+     * </pre>
+     *
+     * @param s the string to be verified.
+     * @return is the string valid.
+     */
+    private static boolean isValid3(String s) {
+        char[] array = s.toCharArray();
+        // Place unpaired parentheses.
+        char[] temp = new char[array.length];
+        int count = 0;
+        for (char aChar : array) {
+            temp[count] = aChar;
+            // If there is a difference of 1 or 2 between the ASCII codes of adjacent
+            // brackets, the brackets match.
+            if (count != 0 && (temp[count] - temp[count - 1] == 1 || temp[count] - temp[count - 1] == 2)) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+        return count == 0;
+    }
+
+    /**
+     * Solution4.
+     * <p>
+     * Use {@code replace()} to eliminate a pair of parentheses in a loop. After the
+     * loop, if the string is empty, it is a legal string.
+     *
+     * @param s the string to be verified.
+     * @return is the string valid.
+     */
+    private static boolean isValid4(String s) {
+        // If the string length is odd, return false.
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        int length = s.length() / 2;
+        for (int i = 0; i < length; i++) {
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        }
+        return s.length() == 0;
+    }
+
+}
