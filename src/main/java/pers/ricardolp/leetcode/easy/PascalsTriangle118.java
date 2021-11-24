86ab2a242e0890be7d7358cb11fa67fb6986e2d7
RicardoLP <861640471@qq.com>
2021-11-24 19:58:41 +0800

    feat: add new solutions.

--- src/main/java/pers/ricardolp/leetcode/easy/PascalsTriangle118.java
+++ src/main/java/pers/ricardolp/leetcode/easy/PascalsTriangle118.java
@@ -5,7 +5,25 @@ import java.util.List;
 /**
  * leetcode_118 : Pascal's Triangle.
  * <p>
- * Detail description
+ * Given an integer {@code numRows}, return the first {@code numRows} of
+ * Pascal's triangle.
+ * <p>
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it as shown:
+ *
+ * <pre>
+ * Example 1:
+ * Input: numRows = 5
+ * Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ *
+ * Example 2:
+ * Input: numRows = 1
+ * Output: [[1]]
+ * </pre>
+ *
+ * Constraints:
+ * <p>
+ * 1 {@code <=} numRows {@code <=} 30
  *
  * @author RicardoLP
  * @since 2021/11/13
