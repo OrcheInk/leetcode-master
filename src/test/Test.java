15f7693cdb8dc67f5a27372198cbe3f8b05b1789
RicardoLP <861640471@qq.com>
2021-12-04 22:05:20 +0800

    feat: add new solutions.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,3 @@
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Test Class.
  *
@@ -8,31 +5,5 @@ import java.util.Set;
  * @since 2021/12/3
  */
 public class Test {
-    private int i;
-
-    public static void main(String[] args) {
-        Test a = new Test();
-        Test b = new Test();
-        a.setI(1);
-        b.setI(1);
-        Set<Test> set = new HashSet<>();
-        set.add(a);
-        set.add(b);
-        System.out.println(a.hashCode() == b.hashCode());
-        System.out.println(a.equals(b));
-        System.out.println(set);
-    }
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
-    }
-
-    public int hashCode() {
-        return i % 10;
-    }
 
 }
